d2dbc2866fd5f2ee6c6556f15bb1c8b553619e44
hannahmariam01 <mariamhannah4@gmail.com>
2025-07-18 08:26:29 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,5 +4,3 @@ st.set_page_config(page_title="Welcome", page_icon="👋", layout="centered")
 
 st.markdown("<h1 style='text-align: center; color: white;'>👋 Welcome to My Streamlit App!</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; font-size: 18px;'>We're glad you're here. Let's explore together!</p>", unsafe_allow_html=True)
-
-st.image(r"C:\Users\Hannah\OneDrive\Desktop\Python (sem 5)\my_photo.jpg", caption="That's me!", width=200)
